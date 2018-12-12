1c6d92c909dc18c4fd950ae51e335ac03cf29c82
sargsyan.mikhail.2017 <sargsyan.mikhail.2017@gmail.com>
2018-12-13 00:12:29 +0200

    Added Login Window

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace CourseWorkWPF1.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Dapino_Summer_Holiday_Palm_tree {
+            get {
+                object obj = ResourceManager.GetObject("Dapino_Summer_Holiday_Palm_tree", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap DrugsMain {
